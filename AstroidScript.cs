551cb26d6b6ca24f263d79a4e707e1873132b853
SamJWCooper <89080620+SamJWCooper@users.noreply.github.com>
2021-08-17 13:59:52 +0100

    Add files via upload

---
+++ AstroidScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstroidScript : MonoBehaviour
+{
+    public Transform target;
+
+    public float moveSpeed;
+
+    bool moveTimeOver;
+
+    Rigidbody2D rb;
+
+    ShipScript shipScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindWithTag("SpaceShip").transform;
+        rb = GetComponent<Rigidbody2D>();
+        moveSpeed = Random.Range(5f, 9f);
+
+        transform.LookAt(target.position);
+
+        StartCoroutine(Despawner());
+        IEnumerator Despawner()
+        {
+            yield return new WaitForSeconds(20);
+            Destroy(gameObject);
+        }
+    }
+    void FixedUpdate()
+    {
+        transform.position += transform.forward * moveSpeed * Time.deltaTime;
+    }
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name == "SpaceShip")
+        {
+            shipScript = target.GetComponent<ShipScript>();
+            shipScript.Die();
+            Debug.Log("DEAD");
+        }
+    }
+}
