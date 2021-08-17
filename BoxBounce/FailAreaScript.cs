b580947ee0dcb4f366b5f9904b2251b29256b87d
SamJWCooper <89080620+SamJWCooper@users.noreply.github.com>
2021-08-17 14:15:17 +0100

    Add files via upload

---
+++ BoxBounce/FailAreaScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FailAreaScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool isOnFailZone;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Box")
+        {
+            isOnFailZone = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Box")
+        {
+            isOnFailZone = false;
+        }
+    }
+}
